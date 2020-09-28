67687b654cc2d86b9575d69329c58f15c6b42e00
Hardik Katehara <hardikkatehara@gmail.com>
2020-09-28 18:03:22 +0530

    dep changes

--- shopmelon/settings.py
+++ shopmelon/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Custom additions
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
